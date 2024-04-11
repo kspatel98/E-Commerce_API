05188c4eb5b06de132923259e34c13de3de312af
Kartik Patel <kartik@git.com>
2024-04-11 12:54:20 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-require("dotenv").config()
+const dotenv=require("dotenv")
+dotenv.config()
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-mongoose.connect(process.env.DATABASE_URL.toString())
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log("Database connected..."))
